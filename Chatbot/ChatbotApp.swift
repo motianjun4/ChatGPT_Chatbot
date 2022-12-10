9ef89b25618ce4a1ed0dcb0ab9da85e1dc7d0bbf
motianjun4 <motianjun4@gmail.com>
2022-12-10 14:45:49 -0500

    Initial Commit

---
+++ Chatbot/ChatbotApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatbotApp.swift
+//  Chatbot
+//
+//  Created by Tianjun Mo on 2022/12/10.
+//
+
+import SwiftUI
+
+@main
+struct ChatbotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Chatbot/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Chatbot
+//
+//  Created by Tianjun Mo on 2022/12/10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
